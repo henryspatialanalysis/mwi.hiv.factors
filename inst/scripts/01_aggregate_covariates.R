fffbfda7338432cf2b578b863334cb8ab2875b44
Nathaniel Henry <nat.henry7@gmail.com>
2025-08-12 20:07:05 -0700

    Aggregate by both GVH and facility.

--- inst/scripts/01_aggregate_covariates.R
+++ inst/scripts/01_aggregate_covariates.R
@@ -25,12 +25,18 @@ dir.create(config$get_dir_path('prepared_data'))
 
 # Load inputs
 pop <- config$read('raw_data', 'population_1km')
+gvh_meta <- config$read('catchments', 'gvh_meta') |>
+  _[, .(gvh_id, closest_facility_id)] |>
+  na.omit() |>
+  unique()
+gvh_catchments <- config$read('catchments', 'gvh_catchments', quiet = TRUE) |>
+  merge(y = gvh_meta, by = 'gvh_id', all.x = TRUE)
 facility_catchments <- config$read('catchments', 'facility_catchments', quiet = TRUE)
 aggregate_results <- config$read('splitting', 'aggregated_results')
 admin_bounds <- config$read("catchments", "admin_bounds", quiet = TRUE)
 
 
-## Assign metro vs. non-metro for each facility catchment ------------------------------->
+## Assign metro vs. non-metro for each facility catchment and GVH ----------------------->
 
 catchments_id_field <- config$get('catchments_id_field')
 facility_catchments[[catchments_id_field]] <- seq_len(nrow(facility_catchments))
@@ -48,6 +54,17 @@ catchment_to_municipality <- facility_catchments |>
 facility_catchments <- facility_catchments |>
   merge(y = catchment_to_municipality, by = catchments_id_field, all.x = TRUE) |>
   dplyr::mutate(in_municipality = as.integer(!is.na(municipality)))
+# Merge facility metadata onto GVH
+facility_to_gvh <- facility_catchments |>
+  sf::st_drop_geometry() |>
+  data.table::as.data.table() |>
+  _[, .(catchment_id, closest_facility_name, municipality, in_municipality)]
+gvh_catchments <- gvh_catchments |>
+  merge(
+    y = facility_to_gvh, by.x = 'closest_facility_id', by.y = 'catchment_id',
+    all.x = TRUE
+  ) |>
+  dplyr::mutate(in_gvh = !is.na(closest_facility_id))
 
 
 ## Create template raster and prepare aggregation table --------------------------------->
@@ -62,25 +79,36 @@ if(file.exists(id_raster_fp)){
 pop <- pop |> terra::crop(id_raster) |> terra::extend(id_raster)
 pop[is.na(pop) & !is.na(id_raster)] <- 0
 
-agg_table_fp <- config$get_file_path('prepared_data', 'aggregation_table')
-if(file.exists(agg_table_fp)){
-  agg_table <- versioning::autoread(agg_table_fp)
-} else {
-  agg_table <- mbg::build_aggregation_table(
-    polygons = facility_catchments,
-    id_raster = id_raster,
-    polygon_id_field = catchments_id_field,
-    verbose = TRUE
+agg_types <- c('facility', 'gvh')
+agg_tables <- vector('list', length = length(agg_types))
+names(agg_tables) <- agg_types
+for(agg_type in agg_types){
+  agg_table_fp <- config$get_file_path(
+    'prepared_data',
+    paste0('aggregation_table_', agg_type)
   )
-  versioning::autowrite(agg_table, file = agg_table_fp)
+  if(file.exists(agg_table_fp)){
+    agg_tables[[agg_type]] <- versioning::autoread(agg_table_fp)
+  } else {
+    agg_tables[[agg_type]] <- mbg::build_aggregation_table(
+      polygons = if(agg_type == 'facility') facility_catchments else gvh_catchments,
+      id_raster = id_raster,
+      polygon_id_field = if(agg_type == 'facility') catchments_id_field else 'gvh_id',
+      verbose = TRUE
+    )
+    versioning::autowrite(agg_tables[[agg_type]], file = agg_table_fp)
+  }
 }
-
 
 ## LOAD AND AGGREGATE EACH COVARIATE ---------------------------------------------------->
 
 cov_names <- names(config$get('covariates'))
 cov_tables <- vector('list', length = length(cov_names))
 names(cov_tables) <- cov_names
+all_cov_tables <- list()
+for(agg_type in agg_types){
+  all_cov_tables[[agg_type]] <- cov_tables
+}
 
 # Load each covariate
 for(cov_name in cov_names){
@@ -97,46 +125,54 @@ for(cov_name in cov_names){
     terra::values(cov_raw)[is.na(terra::values(cov_raw))] <- 0.0
   }
   # Prepare covariate
-  cov_tables[[cov_name]] <- mwi.hiv.factors::aggregate_covariate(
-    cov = cov_raw,
-    template = id_raster,
-    resample_method = 'bilinear',
-    aggregation_table = agg_table,
-    aggregation_cols = catchments_id_field,
-    method = ifelse(cov_name == 'population_1km', 'sum', 'weighted.mean'),
-    weighting_raster = pop
-  )
-  if(cov_name %in% config$get('log_transformed')){
-    cov_tables[[cov_name]]$data <- log1p(cov_tables[[cov_name]]$data)
+  for(agg_type in agg_types){
+    agg_cov_data <- mwi.hiv.factors::aggregate_covariate(
+      cov = cov_raw,
+      template = id_raster,
+      resample_method = 'bilinear',
+      aggregation_table = agg_tables[[agg_type]],
+      aggregation_cols = if(agg_type == 'facility') catchments_id_field else 'gvh_id',
+      method = ifelse(cov_name == 'population_1km', 'sum', 'weighted.mean'),
+      weighting_raster = pop
+    )
+    if(cov_name %in% config$get('log_transformed')){
+      agg_cov_data$data <- log1p(agg_cov_data$data)
+    }
+    data.table::setnames(agg_cov_data, old = 'data', new = cov_name)
+    all_cov_tables[[agg_type]][[cov_name]] <- agg_cov_data
   }
-  data.table::setnames(cov_tables[[cov_name]], old = 'data', new = cov_name)
 }
 
-# Combine all covariates into a single table
-full_data <- facility_catchments |>
-  sf::st_drop_geometry() |>
-  data.table::as.data.table()
+for(agg_type in agg_types){
+  # Combine all covariates into a single table
+  catchments <- if(agg_type == 'facility') facility_catchments else gvh_catchments
+  full_data <- catchments |>
+    sf::st_drop_geometry() |>
+    data.table::as.data.table()
 
-for(cov_name in cov_names){
-  full_data <- merge(
-    x = full_data,
-    y = cov_tables[[cov_name]],
-    by = catchments_id_field,
-    all.x = TRUE
+  for(cov_name in cov_names){
+    full_data <- merge(
+      x = full_data,
+      y = all_cov_tables[[agg_type]][[cov_name]],
+      by = if(agg_type == 'facility') catchments_id_field else 'gvh_id',
+      all.x = TRUE
+    )
+  }
+
+  # Merge on viraemia, prevalence, VLS, and population by catchment
+  agg_level <- if(agg_type == 'facility') 'CLOSEST_FACILITIES' else 'GVH'
+  hiv_by_catchment <- (
+    aggregate_results
+    [ aggregation_level == agg_level, ]
+    [, id := as.integer(id)]
   )
-}
+  merge_id_field <- if(agg_type == 'facility') 'closest_facility_id' else 'gvh_id'
+  if(any(hiv_by_catchment$id != full_data[[merge_id_field]])){
+    stop("Mismatch between closest facility IDs in aggregate results and covariates by catchment")
+  }
+  keep_cols <- grep(pattern = '_mean$', x = colnames(hiv_by_catchment), value = TRUE)
+  full_data <- cbind(full_data, hiv_by_catchment[, keep_cols, with = FALSE])
 
-# Merge on viraemia, prevalence, VLS, and population by catchment
-hiv_by_catchment <- (
-  aggregate_results
-  [ aggregation_level == 'CLOSEST_FACILITIES', ]
-  [, id := as.integer(id)]
-)
-if(any(hiv_by_catchment$id != full_data$closest_facility_id)){
-  stop("Mismatch between closest facility IDs in aggregate results and covariates by catchment")
+  # Save to file
+  config$write(full_data, 'prepared_data', paste0('covariates_by_', agg_type))
 }
-keep_cols <- grep(pattern = '_mean$', x = colnames(hiv_by_catchment), value = TRUE)
-full_data <- cbind(full_data, hiv_by_catchment[, keep_cols, with = FALSE])
-
-# Save to file
-config$write(full_data, 'prepared_data', 'covariates_by_catchment')
